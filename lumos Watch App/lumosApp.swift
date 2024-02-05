4ca28419e96c7e5a377666d8a86454766ea7e77d
Owen ANCELOT <owen.ancelot@la-projets.com>
2024-02-05 22:35:32 +0100

    First commit 🎉

---
+++ lumos Watch App/lumosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lumosApp.swift
+//  lumos Watch App
+//
+//  Created by Owen Ancelot on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct lumos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lumos/lumosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lumosApp.swift
+//  lumos
+//
+//  Created by Owen Ancelot on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct lumosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
